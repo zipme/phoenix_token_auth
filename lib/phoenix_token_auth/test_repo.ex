40c7e21a644725840ae4de463e0a4eefd072d29e
Genki Zhang <genkiwow@gmail.com>
2015-12-28 14:53:50 +0800

    Move TestRepo back to test_helper

--- lib/phoenix_token_auth/test_repo.ex
+++
@@ -1,4 +0,0 @@
-defmodule PhoenixTokenAuth.TestRepo do
-  use Ecto.Repo, otp_app: :phoenix_token_auth
-end
-
--- test/support/ecto_helper.exs
+++ test/support/ecto_helper.exs
@@ -1,3 +1,7 @@
+defmodule PhoenixTokenAuth.TestRepo do
+  use Ecto.Repo, otp_app: :phoenix_token_auth
+end
+
 alias PhoenixTokenAuth.TestRepo
 
 defmodule PhoenixTokenAuth.Case do
